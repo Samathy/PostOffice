3d9e300e1cdfa7f0dbb50b71c7d2990454ab2464
Samathy Barratt <samathy@sbarratt.co.uk>
2018-10-13 19:14:52 +0100

    Added example send for lazy people and testing

---
+++ postoffice_send.py
@@ -0,0 +1,20 @@
+import socket
+
+IP = "127.0.0.1"
+PORT = 7878
+
+message = "Hello World"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((IP, PORT))
+sock.send(message.encode())
+data = sock.recv(1024)
+
+if (str(data) == "OK"):
+   print(str(data))
+
+sock.close()
+
+
+sock.close
